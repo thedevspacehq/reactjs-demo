2cdfcc8c00c9f91e297f9192e538a633864edc92
Eric Hu <huericnan@gmail.com>
2024-09-20 16:53:38 -0400

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,24 @@
 // import Link from "./Link";
 
-export default function Card({ imageUrl, title, link, children }) {
+function Card() {
+  function capitalize(title) {
+    return title
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  }
+
+  const imageUrl = "./image.png";
+  const title = "Lorem ipsum dolor sit amet consectetur adipisicing elit";
+  const description = "Lorem ipsum dolor sit amet consectetur adipisicing elit";
+  const link = "/link-to-article";
+
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden">
       <img src={imageUrl} alt={title} className="w-full h-48 object-cover" />
       <div className="p-6">
-        <h2 className="text-2xl font-bold mb-2">{title}</h2>
-        <p className="text-gray-700 mb-4">{children}</p>
+        <h2 className="text-2xl font-bold mb-2">{capitalize(title)}</h2>
+        <p className="text-gray-700 mb-4">{description}</p>
         <a href={link} className="text-blue-500 hover:text-blue-700">
           Read more
         </a>
@@ -14,6 +26,8 @@ export default function Card({ imageUrl, title, link, children }) {
     </div>
   );
 }
+
+export default Card;
 
 // export default Card;
 
