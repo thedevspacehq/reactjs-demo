24fc5ce408bb0ce6a51350d49faee14a0027a3aa
Eric Hu <huericnan@gmail.com>
2024-09-27 16:46:47 -0400

    update

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 
+function Display({ count }) {
+  return <p>Count: {count}</p>;
+}
+
 export default function Counter() {
   const [count, setCount] = useState(0);
   return (
     <div className="border rounded-md p-4 m-4">
-      <p>Count: {count}</p>
+      <Display count={count} />
       <button
         onClick={() => {
           setCount(count + 1);
