07ff8d37c76511e8cc07423cc076a080b25339db
Eric Hu <huericnan@gmail.com>
2024-10-03 07:38:45 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import Profile from "./components/Profile";
-import { UserProvider } from "./contexts/UserContext";
+// import Profile from "./components/Profile";
+// import { UserProvider } from "./contexts/UserContext";
+
+import { useRef } from "react";
+
+// export default function App() {
+//   return (
+//     <UserProvider>
+//       <Profile />
+//     </UserProvider>
+//   );
+// }
 
 export default function App() {
+  const inputRef = useRef(null);
+
   return (
-    <UserProvider>
-      <Profile />
-    </UserProvider>
+    <>
+      <input type="text" ref={inputRef} />
+      <button
+        onClick={() => {
+          inputRef.current.focus();
+        }}>
+        Click me to focus the input box
+      </button>
+    </>
   );
 }
