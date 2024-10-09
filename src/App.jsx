ed2fa70506851711bad433c36fc86e0ccfd9fc47
Eric Hu <huericnan@gmail.com>
2024-10-08 22:52:14 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,155 @@
+// import { useState, useEffect } from "react";
+
+// export default function App() {
+//   const [user, setUser] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     async function fetchUser() {
+//       const response = await fetch("https://randomuser.me/api/");
+//       const data = await response.json();
+//       setUser(data.results[0]);
+//       setLoading(false);
+//     }
+
+//     fetchUser();
+//   });
+
+//   if (loading) return <div>Loading user...</div>;
+
+//   return (
+//     <div>
+//       <h1>
+//         {user.name.first} {user.name.last}
+//       </h1>
+//       <p>Email: {user.email}</p>
+//       <img src={user.picture.large} alt="User profile" />
+//     </div>
+//   );
+// }
+
+// =============
+
+// import { useState, useEffect } from "react";
+
+// export default function App() {
+//   const [user, setUser] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     async function fetchUser() {
+//       const response = await fetch("https://randomuser.me/api/");
+//       const data = await response.json();
+//       setUser(data.results[0]);
+//       setLoading(false);
+//     }
+
+//     fetchUser();
+//   }, []);
+
+//   if (loading) return <div>Loading user...</div>;
+
+//   return (
+//     <div>
+//       <h1>
+//         {user.name.first} {user.name.last}
+//       </h1>
+//       <p>Email: {user.email}</p>
+//       <img src={user.picture.large} alt="User profile" />
+//     </div>
+//   );
+// }
+
+// ============
+
+// import { useState, useEffect } from "react";
+
+// export default function App() {
+//   const [user, setUser] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     async function fetchUser() {
+//       const response = await fetch("https://randomuser.me/api/");
+//       const data = await response.json();
+//       setUser(data.results[0]);
+//       setLoading(false);
+//     }
+
+//     fetchUser();
+//   }, [count]);
+
+//   if (loading) return <div>Loading user...</div>;
+
+//   return (
+//     <div>
+//       <h1>
+//         {user.name.first} {user.name.last}
+//       </h1>
+//       <p>Email: {user.email}</p>
+//       <img src={user.picture.large} alt="User profile" />
+
+//       <button
+//         onClick={() => {
+//           setCount(count + 1);
+//         }}>
+//         Update Counter
+//       </button>
+//       <p>Count: {count}</p>
+//     </div>
+//   );
+// }
+
+// ================
+
 import { useState, useEffect } from "react";
 
-export default function App() {
-  const [articles, setArticles] = useState([]);
+function User() {
+  const [user, setUser] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    async function fetchArticles() {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/posts"
-        );
-        const data = await response.json();
-        setArticles(data);
-        setLoading(false);
-      } catch (err) {
-        console.error(err);
-      }
+    async function fetchUser() {
+      const response = await fetch("https://randomuser.me/api/");
+      const data = await response.json();
+      setUser(data.results[0]);
+      setLoading(false);
     }
 
-    fetchArticles();
-  });
+    fetchUser();
 
-  if (loading) return <div>Loading articles...</div>;
+    return () => {
+      console.log("User component is unmounted.");
+    };
+  }, []);
+
+  if (loading) return <div>Loading user...</div>;
+
+  return (
+    <>
+      <h1>
+        {user.name.first} {user.name.last}
+      </h1>
+      <p>Email: {user.email}</p>
+      <img src={user.picture.large} alt="User profile" />
+    </>
+  );
+}
+
+export default function App() {
+  const [mount, setMount] = useState(true);
 
   return (
     <div>
-      <h2>Article List</h2>
-      <ul>
-        {articles.map((article) => (
-          <li key={article.id}>
-            <p>- {article.title}</p>
-          </li>
-        ))}
-      </ul>
+      {mount ? <User /> : <p>User component is unmounted.</p>}
+      <button
+        onClick={() => {
+          setMount(!mount);
+        }}>
+        Toggle User
+      </button>
     </div>
   );
 }
