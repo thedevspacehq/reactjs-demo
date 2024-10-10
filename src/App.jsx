093747e6832538e4f78bbcbd2edda2d812284697
Eric Hu <huericnan@gmail.com>
2024-10-10 16:41:43 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -104,51 +104,70 @@
 
 // ================
 
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-function User() {
-  const [user, setUser] = useState([]);
-  const [loading, setLoading] = useState(true);
+// function User() {
+//   const [user, setUser] = useState([]);
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function fetchUser() {
-      const response = await fetch("https://randomuser.me/api/");
-      const data = await response.json();
-      setUser(data.results[0]);
-      setLoading(false);
-    }
+//   useEffect(() => {
+//     async function fetchUser() {
+//       const response = await fetch("https://randomuser.me/api/");
+//       const data = await response.json();
+//       setUser(data.results[0]);
+//       setLoading(false);
+//     }
 
-    fetchUser();
+//     fetchUser();
 
-    return () => {
-      console.log("User component is unmounted.");
-    };
-  }, []);
+//     return () => {
+//       console.log("User component is unmounted.");
+//     };
+//   }, []);
 
-  if (loading) return <div>Loading user...</div>;
+//   if (loading) return <div>Loading user...</div>;
 
-  return (
-    <>
-      <h1>
-        {user.name.first} {user.name.last}
-      </h1>
-      <p>Email: {user.email}</p>
-      <img src={user.picture.large} alt="User profile" />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h1>
+//         {user.name.first} {user.name.last}
+//       </h1>
+//       <p>Email: {user.email}</p>
+//       <img src={user.picture.large} alt="User profile" />
+//     </>
+//   );
+// }
+
+// export default function App() {
+//   const [mount, setMount] = useState(true);
+
+//   return (
+//     <div>
+//       {mount ? <User /> : <p>User component is unmounted.</p>}
+//       <button
+//         onClick={() => {
+//           setMount(!mount);
+//         }}>
+//         Toggle User
+//       </button>
+//     </div>
+//   );
+// }
+
+import { useRef } from "react";
 
 export default function App() {
-  const [mount, setMount] = useState(true);
+  const count = useRef(0);
 
   return (
     <div>
-      {mount ? <User /> : <p>User component is unmounted.</p>}
+      <p>{count.current}</p>
       <button
         onClick={() => {
-          setMount(!mount);
+          count.current = count.current + 1;
+          console.log(count.current);
         }}>
-        Toggle User
+        Add
       </button>
     </div>
   );
